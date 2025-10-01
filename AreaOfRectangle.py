146d3b2547661512a8665bb06a61c5f7c905d6f5
Venkateswaran-A-G <93259660+Venkateswaran-A-G@users.noreply.github.com>
2025-10-01 19:45:49 +0530

    Added Practice Problem 2: Area of the Rectangle

---
+++ AreaOfRectangle.py
@@ -0,0 +1,4 @@
+length=float(input("Enter the length of the rectangle: "))
+breadth=float(input("ENter the breadth of the rectangle: "))
+area = length*breadth
+print("The area of the rectangle is: ", area)
