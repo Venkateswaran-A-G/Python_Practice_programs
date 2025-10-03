686f5201a7bce7abb6ba4ac2ebab91b4e29cf7a1
Venkateswaran-A-G <93259660+Venkateswaran-A-G@users.noreply.github.com>
2025-10-03 12:56:22 +0530

    Added Practice Program 6 : Largest Number

---
+++ LargestNumber.py
@@ -0,0 +1,7 @@
+#Find the largest of three numbers.
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+Num_List = [num1, num2, num3]
+largest = max(Num_List)
+print(f"The largest number is {largest}")
